5dee7ae4396e37026422c1ece8ec9753ac903e34
Lucas Nunes <nunesmlucasg@gmail.com>
2019-03-04 20:55:13 -0500

    annoying.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,19 +7,22 @@ var friendsData = require("../data/friends");
 // ===============================================================================
 
 module.exports = function (app) {
+
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
+        console.log("IN GET API FRIENDS");
     });
 
 
     app.post("/api/friends", function (req, res) {
         friendsData.push(req.body);
+        res.json(true);
         // res.json(true);
     });
 
-    //   app.post("/api/clear", function(req, res) {
-    //     // Empty out the arrays of data
-
-    //     res.json({ ok: true });
-    //   });
+    app.post("/api/clear", function (req, res) {
+        // Empty out the arrays of data
+        friendsData.length = [];
+        res.json({ ok: true });
+    });
 };
